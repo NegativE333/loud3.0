42903b2bcd4386fc7170957ef192ab667a53d3d2
Om Tekade <omtekade914@gmail.com>
2024-06-27 12:01:50 +0530

    db setup and other dependencies add

---
+++ db/schema.ts
@@ -0,0 +1,11 @@
+import { boolean, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+    id: varchar("id").primaryKey(),
+    name: varchar("name").notNull(),
+    email: varchar("email").unique(),
+    password: varchar("password").notNull(),
+    verifyCode: varchar("verifycode").notNull(),
+    verifyCodeExpire: varchar("verifycodeexpire").notNull(),
+    isVerified: boolean("isverified").default(false),
+})
