433c33fb8062de38b8150f42a66cf57cc790aa81
Om Tekade <omtekade914@gmail.com>
2024-06-29 08:09:07 +0530

    responsiveness for sidebar add

--- app/(authorized)/layout.tsx
+++ app/(authorized)/layout.tsx
@@ -1,5 +1,6 @@
 import { Separator } from "@/components/ui/separator";
 import { Sidebar } from "./components/sidebar";
+import { MobileBottomBar } from "./components/mobile-bottom-bar";
 
 
 type Props = {
@@ -10,9 +11,12 @@ const AuthorizedLayout = ({
     children
 }: Props) => {
     return(
-        <div className="h-full bg-gradient-to-bl from-gray-900 to-cyan-200/90">
-            <div className="p-2 h-full w-80">
+        <div className="h-full bg-gradient-to-bl from-gray-900 to-cyan-200/90 relative">
+            <div className="p-2 h-full w-80 hidden md:block">
                 <Sidebar />
+            </div>
+            <div className="md:hidden w-full h-16 absolute bottom-0">
+                <MobileBottomBar />
             </div>
             <div>
                 {children}
