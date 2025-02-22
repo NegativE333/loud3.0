39e26f8caaffc33b6f92f5a635a21bffad3e3fc0
NegativE333 <omtekade914@gmail.com>
2025-02-22 13:00:03 +0530

    search bar fix

--- app/(authorized)/_components/search-bar.tsx
+++ app/(authorized)/_components/search-bar.tsx
@@ -34,11 +34,11 @@ export const SearchBar = () => {
 
     return (
         <div 
-            className="bg-white/50 rounded-md w-full h-fit flex items-center gap-2 p-2"
+            className="bg-white/5 hover:bg-white/8 rounded-md w-full h-fit flex items-center gap-2 p-2.5 transition-colors duration-200 ring-1 ring-white/10"
         >
-            <Search className="size-5 text-cyan-900"/>
+            <Search className="size-5 text-slate-400"/>
             <input
-                className="w-full bg-transparent focus:outline-none focus:border-none"
+                className="w-full bg-transparent focus:outline-none text-slate-200 placeholder:text-slate-400"
                 placeholder={`Search in ${searchTitle}`}
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
@@ -46,7 +46,7 @@ export const SearchBar = () => {
             {search.length > 0 && (
                 <X 
                     onClick={() => setSearch("")}
-                    className="size-5 text-cyan-900 hover:cursor-pointer"
+                    className="size-5 text-slate-400 hover:text-slate-200 transition-colors duration-200 hover:cursor-pointer"
                 />
             )}
         </div>
