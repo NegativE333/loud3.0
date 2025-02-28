a7f77513b2790ac674d71f14ca18f0f146e9ea1d
NegativE333 <omtekade914@gmail.com>
2025-02-28 16:48:14 +0530

    playlist sheet component theme fix

--- app/(authorized)/_components/playlist-sheet.tsx
+++ app/(authorized)/_components/playlist-sheet.tsx
@@ -1,83 +1,60 @@
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { SidebarOptions } from "./sidebar-options";
 import { usePathname, useRouter } from "next/navigation";
 import { UserActionsBtn } from "./user-actions-btn";
 
 type Props = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-export const PlayListSheet = ({
-    children
-}: Props) => {
+export const PlayListSheet = ({ children }: Props) => {
+  const router = useRouter();
+  const pathName = usePathname();
 
-    const router = useRouter();
-    const pathName = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent className="bg-gradient-to-bl from-[#020617] via-[#0F172A] to-[#1E293B]/90 border-none px-3 py-6">
+        <SheetHeader>
+          <SheetTitle className="text-white font-semibold">
+            Play Lists
+          </SheetTitle>
+        </SheetHeader>
 
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                {children}
-            </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle>Play Lists</SheetTitle>
-                    <SheetDescription>
-
-                    </SheetDescription>
-                </SheetHeader>
-
-                <div className="mt-4 flex flex-col items-start gap-3">
-                    <SheetClose className="border-none outline-none focus:outline-none">
-                        <SidebarOptions 
-                            title="All Tracks" 
-                            path="/" 
-                        />
-                    </SheetClose>
-                    <SheetClose>
-                        <SidebarOptions
-                            title="Throwback Tunes"
-                            path="/throwbackTunes"
-                        />
-                    </SheetClose>
-                    <SheetClose>
-                        <SidebarOptions
-                            title="Global Hits"
-                            path="/globalhits"
-                        />                        
-                    </SheetClose>
-                    <SheetClose>
-                        <SidebarOptions
-                            title="Party Starters"
-                            path="/partystarters"
-                        />
-                    </SheetClose>
-                    <SheetClose>
-                        <SidebarOptions
-                            title="Love Ballads"
-                            path="/loveballads"
-                        />
-                    </SheetClose>
-                    <SheetClose>
-                        <SidebarOptions
-                            title="Your favourites"
-                            path="/favourite"
-                        />
-                    </SheetClose>
-                </div>
-                <SheetFooter className="w-full flex items-center justify-center h-full pb-10">
-                    <UserActionsBtn />
-                </SheetFooter>
-            </SheetContent>
-        </Sheet>
-    )
-}
+        <div className="mt-4 flex flex-col items-start gap-3 text-gray-200">
+          <SheetClose className="border-none outline-none focus:outline-none w-full">
+            <SidebarOptions title="All Tracks" path="/" />
+          </SheetClose>
+          <SheetClose>
+            <SidebarOptions title="Throwback Tunes" path="/throwbackTunes" />
+          </SheetClose>
+          <SheetClose>
+            <SidebarOptions title="Global Hits" path="/globalhits" />
+          </SheetClose>
+          <SheetClose>
+            <SidebarOptions title="Party Starters" path="/partystarters" />
+          </SheetClose>
+          <SheetClose>
+            <SidebarOptions title="Love Ballads" path="/loveballads" />
+          </SheetClose>
+          <SheetClose>
+            <SidebarOptions title="Your favourites" path="/favourite" />
+          </SheetClose>
+        </div>
+        <SheetFooter className="w-full flex items-center justify-center h-full pb-10">
+          <SheetClose>
+            <UserActionsBtn />
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
--- app/(authorized)/_components/user-actions-btn.tsx
+++ app/(authorized)/_components/user-actions-btn.tsx
@@ -1,37 +1,31 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { LogOut, User } from "lucide-react"
-import { signOut, useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { LogOut, User } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export const UserActionsBtn = () => {
-    const { data } = useSession();
-    const router = useRouter();
-    return (
-        <div>
-            <Separator
-                className="bg-gray-700 bg-opacity-20 h-0.5 rounded-full my-4 w-full"
-            />
-            <div className="flex gap-4 items-center">
-                <Button
-                    onClick={() => router.push("/profile")}
-                    className="bg-white bg-opacity-40 text-gray-800 hover:bg-white hover:bg-opacity-60 hover:shadow-sm max-w-[55%]"
-                >
-                    <User
-                        className="size-4 mr-1"
-                    />
-                    <p className="truncate">
-                        {data?.user.name}
-                    </p>
-                </Button>
-                <Button
-                    className="bg-white bg-opacity-40 text-gray-800 hover:bg-white hover:bg-opacity-60 hover:shadow-sm ml-auto"
-                    onClick={() => signOut()}
-                >
-                    <LogOut className="size-4 mr-1" />
-                    Sign Out
-                </Button>
-            </div>
-        </div>
-    )
-}
+  const { data } = useSession();
+  const router = useRouter();
+  return (
+    <div>
+      <Separator className="bg-gray-700 bg-opacity-20 h-0.5 rounded-full my-4 w-full" />
+      <div className="flex gap-4 items-center w-full">
+        <Button
+          onClick={() => router.push("/profile")}
+          className="bg-gray-900/50 text-gray-200 hover:bg-gray-700/60 hover:text-white max-w-[55%] transition-colors"
+        >
+          <User className="size-4 mr-1" />
+          <p className="truncate">{data?.user.name}</p>
+        </Button>
+        <Button
+          className="bg-gray-800/50 text-gray-200 hover:bg-gray-700/60 hover:text-white transition-colors ml-auto"
+          onClick={() => signOut()}
+        >
+          <LogOut className="size-4 mr-1" />
+          Sign Out
+        </Button>
+      </div>
+    </div>
+  );
+};
