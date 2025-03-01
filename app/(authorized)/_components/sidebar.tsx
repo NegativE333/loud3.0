34c855ed98de30946e5d53e8ea6a858a3927cc85
NegativE333 <omtekade914@gmail.com>
2025-03-01 10:55:03 +0530

    padding and loading screen ui fix

--- app/(authorized)/_components/sidebar.tsx
+++ app/(authorized)/_components/sidebar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import { MusicPlayer } from "./music-player";
 import { UserActionsBtn } from "./user-actions-btn";
 import { SidebarOptions } from "./sidebar-options";
-import {useMedia} from 'use-media';
+import { useMedia } from "use-media";
 import Link from "next/link";
 import { SearchBar } from "./search-bar";
 import { Nunito } from "next/font/google";
@@ -13,57 +13,40 @@ import { cn } from "@/lib/utils";
 const title = Nunito({ subsets: ["latin"] });
 
 export const Sidebar = () => {
-    const isMd = useMedia({ maxWidth: "767px" });
-    return (
-        <div className="h-[98%] w-80 bg-[#020617]/40 rounded-sm px-4 py-2.5 fixed flex flex-col">
-            <div className="flex items-center w-full">
-                <Link 
-                    href={"/"}
-                    className={cn("text-4xl font-semibold text-blue-50", title.className)}
-                >
-                    loud
-                </Link>
-            </div>
-            <Separator
-                className="bg-slate-600/20 h-0.5 rounded-full mb-2 mt-1"
-            />
-            <div className="my-3">
-                <SearchBar />
-            </div>
-            <div className="flex flex-col items-start justify-start gap-1">
-                <SidebarOptions 
-                    title="All Tracks"
-                    path="/"
-                />
-                <SidebarOptions 
-                    title="Throwback Tunes"
-                    path="/throwbackTunes"
-                />
-                <SidebarOptions 
-                    title="Global Hits"
-                    path="/globalhits"
-                />
-                <SidebarOptions 
-                    title="Party Starters"
-                    path="/partystarters"
-                />
-                <SidebarOptions 
-                    title="Love Ballads"
-                    path="/loveballads"
-                />
-                <SidebarOptions 
-                    title="Favourite Tunes"
-                    path="/favourite"
-                />
-            </div>
-            {!isMd && (
-                <div className="w-full left-0 mt-auto">
-                    <MusicPlayer />
-                </div>
-            )}
-            {/* <div className="flex flex-col mt-4">
+  const isMd = useMedia({ maxWidth: "767px" });
+  return (
+    <div className="h-[98%] w-80 bg-[#020617]/40 rounded-sm px-4 py-2.5 fixed flex flex-col">
+      <div className="flex items-center w-full">
+        <Link
+          href={"/"}
+          className={cn(
+            "text-4xl font-semibold bg-gradient-to-r from-cyan-300 to-blue-300 bg-clip-text text-transparent",
+            title.className
+          )}
+        >
+          loud
+        </Link>
+      </div>
+      <Separator className="bg-slate-600/20 h-0.5 rounded-full mb-2 mt-1" />
+      <div className="my-3">
+        <SearchBar />
+      </div>
+      <div className="flex flex-col items-start justify-start gap-1">
+        <SidebarOptions title="All Tracks" path="/" />
+        <SidebarOptions title="Throwback Tunes" path="/throwbackTunes" />
+        <SidebarOptions title="Global Hits" path="/globalhits" />
+        <SidebarOptions title="Party Starters" path="/partystarters" />
+        <SidebarOptions title="Love Ballads" path="/loveballads" />
+        <SidebarOptions title="Favourite Tunes" path="/favourite" />
+      </div>
+      {!isMd && (
+        <div className="w-full left-0 mt-auto">
+          <MusicPlayer />
+        </div>
+      )}
+      {/* <div className="flex flex-col mt-4">
                 <UserActionsBtn />
             </div> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
--- app/(authorized)/partystarters/loading.tsx
+++ app/(authorized)/partystarters/loading.tsx
@@ -1,4 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { Hind } from 'next/font/google';
 import { SkeletonLoading } from "../_components/skeleton-loading";
@@ -7,7 +6,7 @@ const heading = Hind({ subsets: ['latin'], weight: '500' });
 
 const Loading = () => {
     return (
-        <div className="lg:ml-8 pb-24 md:pb-0">
+        <div className="pb-24 md:pb-0">
             <h1
                 className={cn("text-3xl md:text-4xl font-bold bg-gradient-to-r from-white via-white/70 to-white inline-block text-transparent bg-clip-text", heading.className)}
             >
